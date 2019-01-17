b20fe77f4d7f3975407180e5b6ed1398a63eb8f1
g0sseyn <39993467+g0sseyn@users.noreply.github.com>
2019-01-17 18:12:12 +0100

    Add files via upload

--- model/Manager.php
+++ model/Manager.php
@@ -4,22 +4,13 @@ class Manager
 	protected $db;
 	protected function dbconnect() {
 		if(!$this->db) {
-			$this->db =  new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
+			$this->db =  new PDO('mysql:host=aguillauiugoss.mysql.db;dbname=aguillauiugoss;charset=utf8', '', '');
 		}
-		
+	
+	}
+	public function __construct(){
+		if(!$this->db) {
+			$this->db =  new PDO('mysql:host=aguillauiugoss.mysql.db;dbname=aguillauiugoss;charset=utf8', '', '');
+		}
 	}
 }
-/*
-function addAdmin(){
-
-		$pseudo = 'admin';
-		$pass_hache = password_hash('admin', PASSWORD_DEFAULT);
-
-$db = new PDO('mysql:host=localhost;dbname=projet4;charset=utf8', 'root', '');
-$req = $db->prepare('INSERT INTO admin(pseudo, pass) VALUES(?,?)');
-$b=$req->execute(array($pseudo,$pass_hache));
-$a=$db->prepare('INSERT INTO comments(news_id, author, comment, comment_date) VALUES(?, ?, ?, NOW())');	
-$affectedLines = $a->execute(array('1', $pseudo, $pass_hache));
-}
-addAdmin(); */
-
